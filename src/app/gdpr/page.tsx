623e47e64cfbc2dc75f6c49443ca592b93990bfb
Torencen <vanko.tim@gmail.com>
2024-09-26 12:34:57 +0200

    initial

---
+++ src/app/gdpr/page.tsx
@@ -0,0 +1,7 @@
+import Typography from "@mui/material/Typography";
+
+export default function GDPR() {
+  return (
+    <Typography> Stranka na GDPR </Typography>
+  );
+}
---
+++ src/app/podmienky/page.tsx
@@ -0,0 +1,7 @@
+import Typography from "@mui/material/Typography";
+
+export default function TermsConditions() {
+  return (
+    <Typography> Podmienky pouzivania tohto webu </Typography>
+  );
+}
